33cf0caac90794c5c8db5ccfc404338199dbfcb3
bala <bala@ost.com>
2020-02-28 19:00:09 +0530

    ui changes

--- api/data/jbgc68hybe0b.js
+++ api/data/jbgc68hybe0b.js
@@ -6,7 +6,7 @@ export default makeDataFn(
         query:
             'index=main sourcetype="ethereum:transaction:event" address=0x3FE42c2842377a5F4dc0E521720fb0f0048Baf9A "event.signature"="Transfer(address,address,uint256)" \n| eval from=mvindex(\'event.params{}.value\', 0), to=mvindex(\'event.params{}.value\', 1), value=mvindex(\'event.params{}.value\', 2) \n| table transactionHash from to value \n| eval value = value / pow(10, 18) \n| stats sum(value) as totalValue by to \n| sort - totalValue \n| lookup pepo_users tokenholder_address as to\n| search name != RESERVE NOT name="The Pepo App" NOT name="Pepo App" \n| head 10 | table name totalValue\n',
         queryParameters: {
-            earliest: '2020-02-13T00:00:00.000',
+            earliest: '2020-02-28T00:00:00.000',
             latest: 'now',
         },
         refresh: 60,
