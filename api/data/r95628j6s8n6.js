693146aa1b3669090ea4b11a852a58853a8c455d
sunil <sunil@hem.com>
2020-03-02 16:48:56 +0530

    Increased limit on video search query

--- api/data/r95628j6s8n6.js
+++ api/data/r95628j6s8n6.js
@@ -15,7 +15,7 @@ export default makeDataFn(
         | rename data.activity.video.video_url as video 
         | table video 
         | append 
-            [ search index=main earliest=-4h 
+            [ search index=main earliest=-1d
             | rename data.activity.video.tags{} AS tagvalue|eval tagValueLower=lower(tagvalue) 
             | where (tagValueLower LIKE "ethcc") or (tagValueLower LIKE "paris") or (tagValueLower LIKE "bageth") or (tagValueLower LIKE "franceblockchainweek")
             | stats sum(data.activity.video.total_contribution_amount) as contrib by data.activity.video.video_url
